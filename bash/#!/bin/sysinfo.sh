786ee6cc838e0770e6126b22b937e0688f5eebee
RajanCOMP2101 <105621121+RajanCOMP2101@users.noreply.github.com>
2022-06-20 12:18:33 -0400

    Delete sysinfo.sh

--- bash/#!/bin/sysinfo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-Hostnmame          :   Rajan
-LAN Address        :   192.168.56.1   
-LAN Name           :   enp0s3
-External IP        :   172.16.0.1
-Router IP          :   VMnet12
-Router Name        :   jashan
-Network Num        :   192.168.56.0
-network name       :   Rajan
